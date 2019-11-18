9b982d83d8cf2f9ab42a7050bc4d20614653605c
arturu <arturu@arturu.it>
2019-11-19 00:03:38 +0100

    Webpack: add scss

---
+++ src/js/index.js
@@ -0,0 +1,1 @@
+import '../../node_modules/bootstrap-italia/src/scss/bootstrap-italia.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,50 @@
 var path = require('path');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/js/index.js',
 
   output: {
     path: path.resolve(__dirname, 'assets'),
-    filename: "bundle.js",
-    publicPath: "/assets/"
-  }
+    filename: "js/bundle.js",
+    publicPath: "/assets/",
+  },
+
+  devtool: "source-map",
+
+  module: {
+    rules: [{
+      test: /\.(sa|sc|c)ss$/i,
+      use: [
+        {
+          loader: MiniCssExtractPlugin.loader,
+          options: {
+            hmr: process.env.NODE_ENV === 'development',
+            reloadAll: true,
+          },
+        },
+        {
+          loader: 'css-loader',
+          options: {
+            sourceMap: true,
+          }
+        },
+        {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: true,
+          },
+        },
+      ],
+    }],
+  },
+
+  plugins: [
+    new UglifyJsPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+      chunkFilename: 'css/[id].css'
+    }),
+  ],
 };
