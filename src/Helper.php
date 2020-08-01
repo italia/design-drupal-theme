32a4c8f6a8d9d924f7163117ac276e3b1baa40aa
arturu <arturu@arturu.it>
2020-08-01 15:28:59 +0200

    Social: optimize code

---
+++ src/Helper.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Drupal\bootstrap_italia;
+
+class Helper {
+
+  public static function getSocialItems(): array
+  {
+    return [
+      'Twitter',
+      'Facebook',
+      'YouTube',
+      'Instagram',
+      // 'Telegram', //manca icona
+      'WhatsApp',
+      // 'RSS', //manca icona
+    ];
+  }
+
+}
