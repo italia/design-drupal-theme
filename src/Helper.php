7a1eb185f7df74dda05624545b7bff17ef489c38
arturu <arturu@arturu.it>
2020-08-19 12:52:56 +0200

    Add sidebar settings and menu theme on sidebar first

--- src/Helper.php
+++ src/Helper.php
@@ -22,4 +22,22 @@ class Helper {
     ];
   }
 
+  public static function getColorsName($withKeys=false): array
+  {
+    if ($withKeys) {
+      return [
+        'primary' => 'Primary ',
+        'secondary' => 'Secondary',
+        'success' => 'Success',
+        'info' => 'Info',
+        'warning' => 'Warning',
+        'danger' => 'Danger',
+        'light' => 'Light',
+        'white' => 'White',
+        'dark' => 'Dark',
+      ];
+    }
+
+    return  [ 'primary ', 'secondary', 'success', 'info', 'warning', 'danger', 'light', 'white', 'dark' ];
+  }
 }
