ce797ff84267b2d6edf2698efb420195da72283d
arturu <arturu@arturu.it>
2020-09-01 11:31:30 +0200

    Fix paragraphs module: 'Configuration objects provided by bootstrap_italia_paragraph have unmet dependencies: field.field.paragraph.impostazioni.field_parallasse (field.storage.paragraph.field_parallasse)'

--- var/bin/start-dev-test.sh
+++ var/bin/start-dev-test.sh
@@ -2,48 +2,48 @@
 
 # https://ddev.readthedocs.io/en/latest/#intro-to-ddev-local
 
-# Make test folder
+echo 'Make test folder'
 mkdir test-bootstrap-italia
 cd test-bootstrap-italia || exit
 
-# Config
+echo 'Configuration'
 ddev config --project-type=drupal8 --docroot=web --create-docroot
 
-# Run docker container
+echo 'Run docker container'
 ddev start
 
-# Download drupal
+echo 'Download drupal and drush'
 ddev composer create "'drupal/recommended-project':'^8.9.0 < 9.0'"
 ddev composer require drush/drush
 #ddev composer require 'drupal/console:>=1.9'
 
-# Install drupal
+echo 'Install drupal'
 ddev exec drush -y site:install --locale=it
 
-# Install end Enable dependencies
+echo 'Install end Enable dependencies'
 ddev exec drush -y pm:enable inline_form_errors responsive_image
-ddev composer require drupal/components drupal/ui_patterns drupal/focal_point
-ddev exec drush -y pm:enable components ui_patterns ui_patterns_layouts ui_patterns_library ui_patterns_views focal_point
+ddev composer require drupal/components drupal/ui_patterns
+ddev exec drush -y pm:enable components ui_patterns ui_patterns_layouts ui_patterns_library ui_patterns_views
 
-# Language settings
+echo 'Language settings'
 ddev exec drush -y en locale
 ddev exec drush -y language-add en
 
-# Download bootstrap_italia
+echo 'Download bootstrap_italia'
 ddev composer require 'drupal/bootstrap_italia:0.x-dev'
 
-# Copy sub-theme to destination folder
+echo 'Copy sub-theme to destination folder'
 ddev exec mkdir /var/www/html/web/themes/custom/
 ddev exec cp -r /var/www/html/web/themes/contrib/bootstrap_italia/var/starter_kits/italiagov /var/www/html/web/themes/custom/
 
-# Enable themes
+echo 'Enable themes'
 ddev exec drush -y theme:enable bootstrap_italia
 ddev exec drush -y theme:enable italiagov
 
-# Set default theme
+echo 'Set default theme'
 ddev exec drush -y config:set system.theme default italiagov
 
-# Install assets
+echo 'Install assets'
 ddev exec npm install --prefix themes/custom/italiagov/
 ddev exec npm run build:prod --prefix themes/custom/italiagov/
 
@@ -51,10 +51,17 @@ ddev exec npm run build:prod --prefix themes/custom/italiagov/
 #ddev exec drush -y config:set system.site name \'Bootstrap Italia Test\'
 #dde#v exec drush -y config:set system.site slogan \'Bootstrap Italia Test v0.x-dev\'
 
-# Cache rebuild
+echo 'Enable optional modules'
+ddev composer require drupal/focal_point
+ddev exec drush -y pm:enable focal_point bootstrap_italia_image_styles
+
+ddev composer require drupal/paragraphs drupal/field_group drupal/imce
+ddev exec drush -y pm:enable paragraphs field_group ui_patterns_field_group imce bootstrap_italia_paragraphs
+
+echo 'Cache rebuild'
 ddev exec drush cr
 
-# Push ssh key in to container
+echo 'Push ssh public key in to container, if you have many keys press CRTL+C after first push'
 ddev auth ssh
 
 # ddev info
