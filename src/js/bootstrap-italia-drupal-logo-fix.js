1725d1bf822e7d733317578a2ac4abcd5358a5b8
arturu <arturu@arturu.it>
2022-05-29 10:38:52 +0200

    Search button in central header.

--- src/js/bootstrap-italia-drupal-logo-fix.js
+++ src/js/bootstrap-italia-drupal-logo-fix.js
@@ -1,3 +1,4 @@
+// is-sticky fix. Sticky clones the "id" attributes.
 (function () {
   'use strict';
 
@@ -6,11 +7,24 @@
       mutationList.forEach(function(mutation) {
         if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
           if (mutation.target.className.includes('is-sticky')) {
+            // fix for logo
             let logoElementSelector = '.it-brand-wrapper.cloned-element .icon'
             let stickyViewbox = document.querySelector(logoElementSelector).dataset.stickyViewbox
             if (stickyViewbox) {
               document.querySelector(logoElementSelector)
                 .setAttribute('viewBox', stickyViewbox);
+            }
+            // fix for search button
+            let searchButtonSelector = '.it-search-wrapper.cloned-element a.search-link'
+            let searchButtonElement = document.querySelector(searchButtonSelector)
+            let searchModalSelector = '.it-search-wrapper.cloned-element div#modal-header-center-search'
+            let searchModal = document.querySelector(searchModalSelector)
+
+            if (searchButtonElement && searchModal) {
+              document.querySelector(searchModalSelector)
+                .setAttribute('id', 'modal-header-center-search-cloned');
+              document.querySelector(searchButtonSelector)
+                .setAttribute('data-bs-target', '#modal-header-center-search-cloned');
             }
           }
         }
