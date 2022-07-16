b40bba64c11e951b5135c8864bb2f7048aa5b487
arturu <arturu@arturu.it>
2022-07-16 16:48:45 +0200

    Listen function (WIP)

--- src/js/bootstrap-italia-drupal-speech.js
+++ src/js/bootstrap-italia-drupal-speech.js
@@ -1,54 +1,82 @@
-(function () {
+(function (Drupal, once) {
   'use strict';
 
-  if ('speechSynthesis' in window) {
-    // First we initialize new SpeechSynthesisUtterance object
-    let tts    = new SpeechSynthesisUtterance();
+  Drupal.behaviors.speechSynthesisUtterance = {
+    attach: function (context, settings) {
+      once('speechSynthesisUtterance', '#it-block-italiagov-content', context).forEach(function (element) {
 
-    // Setting the Speech Language
-    // lang: If unset, the app's (i.e. the <html> lang value) lang will be used, or the user-agent default if that is unset too.
-    tts.lang   = 'it-IT';
+        let lang = navigator.language
+        let voiceIndex = 1
+        let rate = 0.9
 
-    let speechvoices = []; // global array of available voices
+        const speak = async text => {
+          if (!speechSynthesis) {
+            return
+          }
+          const message = new SpeechSynthesisUtterance(text)
+          message.voice = await chooseVoice()
+          message.rate = rate
+          speechSynthesis.speak(message)
+        }
 
-    // To get the list of voices using getVoices() function
-    speechvoices = window.speechSynthesis.getVoices();
-    tts.voice  = speechvoices[25]; // only for Firefox??
+        const getVoices = () => {
+          return new Promise(resolve => {
+            let voices = speechSynthesis.getVoices()
+            if (voices.length) {
+              resolve(voices)
+              return
+            }
+            speechSynthesis.onvoiceschanged = () => {
+              voices = speechSynthesis.getVoices()
+              resolve(voices)
+            }
+          })
+        }
 
-    // rate
-    tts.rate   = 0.9;
+        const chooseVoice = async () => {
+          const voices = (await getVoices()).filter(voice => voice.lang == lang)
 
-    //SETTING THE PLAY CONTROL
-    //first we get the value of the textarea or document
-    document.getElementById("speak").addEventListener("click", () => {
-      tts.text = document.getElementById("lines").value;
-      //then we implement the speechsynthesis instance
-      window.speechSynthesis.speak(tts);
-    });
+          return new Promise(resolve => {
+            resolve(voices[voiceIndex])
+          })
+        }
 
-    // optionals // TODO more tag?
-    //PAUSE
-    document.getElementById("pause").addEventListener("click", () => {
-      // Pause the speechSynthesis instance
-      window.speechSynthesis.pause();
-    });
+        const extractTextFromNode = el => {
+          let n, a=[], walk=document.createTreeWalker(el,NodeFilter.SHOW_TEXT,null,false);
+          while(n=walk.nextNode()) a.push(n.textContent);
+          return a.join(' ');
+        }
 
-    //RESUME
-    document.getElementById("resume").addEventListener("click", () => {
-      // Resume the paused speechSynthesis instance
-      window.speechSynthesis.resume();
-    });
+        //SETTING THE PLAY CONTROL
+        //first we get the value of the textarea or document
+        document.getElementById("it-share-action-speak").addEventListener("click", () => {
+          let classToRead = document.getElementById("it-share-action-speak").dataset.biRead;
+          let elementToRead = document.querySelector('.'+classToRead);
+          speak(extractTextFromNode(elementToRead));
+        });
 
-    //CANCEL
-    document.querySelector("cancel").addEventListener("click", () => {
-    // Cancel the speechSynthesis instance
-      window.speechSynthesis.cancel();
-    });
-    // end optionals
+/*        // optionals // TODO more tag?
+        //PAUSE
+        document.getElementById("pause").addEventListener("click", () => {
+          // Pause the speechSynthesis instance
+          window.speechSynthesis.pause();
+        });
 
-  }
-  else{
-    document.write(Drupal.t("Browser not supported"))
-  }
+        //RESUME
+        document.getElementById("resume").addEventListener("click", () => {
+          // Resume the paused speechSynthesis instance
+          window.speechSynthesis.resume();
+        });
 
-})(Drupal);
+        //CANCEL
+        document.querySelector("cancel").addEventListener("click", () => {
+          // Cancel the speechSynthesis instance
+          window.speechSynthesis.cancel();
+        });
+        // end optionals*/
+
+      });
+    }
+  };
+
+})(Drupal, once);
