7ff30becf7c9532ba7c405ed5e3ed8a4b05055f1
arturu <arturu@arturu.it>
2022-08-16 14:36:34 +0200

    Added views list and view list display

---
+++ modules/bootstrap_italia_views_list/src/Plugin/views/style/ListStyle.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Drupal\bootstrap_italia_views_list\Plugin\views\style;
+
+use Drupal\core\form\FormStateInterface;
+use Drupal\views\Plugin\views\style\StylePluginBase;
+
+/**
+ * Style plugin to render swiper carousel.
+ *
+ * @ingroup views_style_plugins
+ *
+ * @ViewsStyle(
+ *   id = "bootstrap_italia_views_list_style",
+ *   title = @Translation("Bootstrap Italia List component"),
+ *   help = @Translation("Render a Bootstrap Italia List."),
+ *   theme = "views_bootstrap_italia_views_list",
+ *   display_types = { "normal" }
+ * )
+ */
+class ListStyle extends StylePluginBase {
+
+  /**
+   * Does this Style plugin allow Row plugins?
+   *
+   * @var bool
+   */
+  protected $usesRowPlugin = TRUE;
+
+  /**
+   * Does the Style plugin support grouping of rows?
+   *
+   * @var bool
+   */
+  protected $usesGrouping = FALSE;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function defineOptions() {
+    $options = parent::defineOptions();
+    $options['list_settings'] = ['default' => []];
+    return $options;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildOptionsForm(&$form, FormStateInterface $form_state) {
+    parent::buildOptionsForm($form, $form_state);
+
+  }
+
+}
+
