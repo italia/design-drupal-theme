e8a841996ed41cc5c2ec8d7c52bdafe5ad4867c4
arturu <arturu@arturu.it>
2024-12-02 12:38:45 +0100

    fix: various lint code fix

--- js/bootstrap-italia-drupal-sticky-fix.js
+++ js/bootstrap-italia-drupal-sticky-fix.js
@@ -12,10 +12,10 @@
               if (mutation.target.className.includes('is-sticky')) {
 
                 // fix for logo
-                let logoElementSelector = '.it-brand-wrapper.cloned-element .icon'
-                let logoElement = document.querySelector(logoElementSelector)
+                let logoElementSelector = '.it-brand-wrapper.cloned-element .icon';
+                let logoElement = document.querySelector(logoElementSelector);
                 if (logoElement) {
-                  let stickyViewboxData = logoElement.dataset.stickyViewbox
+                  let stickyViewboxData = logoElement.dataset.stickyViewbox;
                   if (stickyViewboxData) {
                     document.querySelector(logoElementSelector)
                       .setAttribute('viewBox', stickyViewboxData);
@@ -23,24 +23,24 @@
                 }
 
                 // fix for search button
-                let searchButtonSelector = '.it-search-wrapper.cloned-element a.search-link'
-                let searchButtonElement = document.querySelector(searchButtonSelector)
-                let searchModalSelector = '.it-search-wrapper.cloned-element div#modal-header-center-search'
-                let searchModalElement = document.querySelector(searchModalSelector)
+                let searchButtonSelector = '.it-search-wrapper.cloned-element a.search-link';
+                let searchButtonElement = document.querySelector(searchButtonSelector);
+                let searchModalSelector = '.it-search-wrapper.cloned-element div#modal-header-center-search';
+                let searchModalElement = document.querySelector(searchModalSelector);
 
                 if (searchButtonElement && searchModalElement) {
-                  let newID = 'modal-header-center-search-cloned'
+                  let newID = 'modal-header-center-search-cloned';
                   document.querySelector(searchModalSelector)
                     .setAttribute('id', newID);
                   document.querySelector(searchButtonSelector)
                     .setAttribute('data-bs-target', '#' + newID);
 
                   searchModalElement.addEventListener('shown.bs.modal', () => {
-                    document.getElementById('toolbar-administration')?.classList.add('d-none')
-                  })
+                    document.getElementById('toolbar-administration')?.classList.add('d-none');
+                  });
                   searchModalElement.addEventListener('hidden.bs.modal', () => {
-                    document.getElementById('toolbar-administration')?.classList.remove('d-none')
-                  })
+                    document.getElementById('toolbar-administration')?.classList.remove('d-none');
+                  });
                 }
               }
             }
@@ -50,12 +50,12 @@
 
       once('stickyFix', '.it-header-wrapper').forEach(function (headerWrapperObserverTarget) {
         //let headerWrapperObserverTarget = document.querySelector(".it-header-wrapper")
-        let headerNavWrapper = document.getElementById("header-nav-wrapper")
+        let headerNavWrapper = document.getElementById("header-nav-wrapper");
         if (headerWrapperObserverTarget && headerNavWrapper) {
           headerWrapperObserver.observe(
             headerWrapperObserverTarget,
             {attributes: true}
-          )
+          );
         }
       });
     }
--- js/bootstrap-italia-enable-all-tooltip.js
+++ js/bootstrap-italia-enable-all-tooltip.js
@@ -1,11 +1,9 @@
 (function (Drupal, once, bootstrap) {
-
   Drupal.behaviors.enableAllTooltip = {
     attach: function (context) {
       once('enableAllTooltip', '[data-bs-toggle="tooltip"]', context).forEach(function (element) {
-        return new bootstrap.Tooltip(element)
+        return new bootstrap.Tooltip(element);
       });
-    }
+    },
   };
-
 })(Drupal, once, bootstrap);
