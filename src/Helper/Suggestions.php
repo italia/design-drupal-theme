7f1400059ab3084f6b020f5400759d952199bf7f
arturu <arturu@arturu.it>
2026-02-05 16:46:21 +0100

    feat: compliance phpstan level 9 in Suggestions.php

--- src/Helper/Suggestions.php
+++ src/Helper/Suggestions.php
@@ -25,25 +25,48 @@ class Suggestions {
    *   Specific hook.
    */
   public static function form(array &$suggestions, array $variables, ?string $hook = NULL): void {
-    // Add a suggestion based on the element type.
-    if (isset($variables['element']['#type'])) {
-      $suggestions[] = $variables['theme_hook_original'] . '__type__' . $variables['element']['#type'];
+    /** @var string $hook_original */
+    $hook_original = $variables['theme_hook_original'];
+
+    /** @var array<string, mixed> $element */
+    $element = $variables['element'];
+
+    if (!empty($element)) {
+
+      // Add a suggestion based on the element type.
+      if (isset($element['#type']) && is_string($element['#type'])) {
+        $element_type = $element['#type'];
+
+        $suggestions[] = $hook_original . '__type__' . $element_type;
+
+        if (!is_null($hook)) {
+          $suggestions[] = $hook . '__type__' . $element_type;
+        }
+      }
+
+      // Add a suggestion based on the element id.
+      if (isset($element['#id']) && is_string($element['#id'])) {
+        $element_id = self::sanitize($element['#id']);
+
+        $suggestions[] = $hook_original . '__id__' . $element_id;
+
+        if (!is_null($hook)) {
+          $suggestions[] = $hook . '__id__' . $element_id;
+        }
+      }
+
+      // Add a suggestion based on the element name.
+      if (isset($element['#name']) && is_string($element['#name'])) {
+        $element_name = self::sanitize($element['#name']);
+
+        $suggestions[] = $hook_original . '__name__' . $element_name;
+
+        if (!is_null($hook)) {
+          $suggestions[] = $hook . '__name__' . $element_name;
+        }
+      }
     }
-    if (isset($variables['element']['#type']) && !is_null($hook)) {
-      $suggestions[] = $hook . '__type__' . $variables['element']['#type'];
-    }
-    if (isset($variables['element']['#id'])) {
-      $suggestions[] = $variables['theme_hook_original'] . '__id__' . self::sanitize($variables['element']['#id']);
-    }
-    if (isset($variables['element']['#id']) && !is_null($hook)) {
-      $suggestions[] = $hook . '__id__' . self::sanitize($variables['element']['#id']);
-    }
-    if (isset($variables['element']['#name'])) {
-      $suggestions[] = $variables['theme_hook_original'] . '__name__' . $variables['element']['#name'];
-    }
-    if (isset($variables['element']['#name']) && !is_null($hook)) {
-      $suggestions[] = $hook . '__name__' . $variables['element']['#name'];
-    }
+
   }
 
   /**
@@ -58,45 +81,83 @@ class Suggestions {
    * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
    */
   public static function block(array &$suggestions, array $variables): void {
-    $content = $variables['elements']['content'];
+    /** @var string $hook_original */
+    $hook_original = $variables['theme_hook_original'];
+
+    /** @var array<string, mixed> $elements */
+    $elements = $variables['elements'];
+
+    /** @var array<string, mixed> $content */
+    $content = $elements['content'];
+
+    // Bundle and view mode suggestions.
     if (isset($content['#block_content']) and $content['#block_content'] instanceof BlockContentInterface) {
       $bundle = $content['#block_content']->bundle();
+      if (!empty($bundle)) {
+        $suggestions[] = 'block__' . $bundle;
+      }
+
+      /** @var string $view_mode */
       $view_mode = $content['#view_mode'];
-      $suggestions[] = 'block__' . $bundle;
-      $suggestions[] = 'block__' . $view_mode;
-      $suggestions[] = 'block__' . $bundle . $view_mode;
+      if (!empty($view_mode)) {
+        $suggestions[] = 'block__' . $view_mode;
+      }
+
+      if (!empty($bundle) && !empty($view_mode)){
+        $suggestions[] = 'block__' . $bundle . $view_mode;
+      }
     }
 
-    if (!empty($variables['elements']['#id'])) {
+    if (!empty($elements['#id'])) {
+      /** @var string|int $elements_id_original */
+      $elements_id_original = $elements['#id'];
+
       /** @var \Drupal\block\BlockInterface $block */
       $block = \Drupal::entityTypeManager()
         ->getStorage('block')
-        ->load($variables['elements']['#id']);
+        ->load($elements_id_original);
 
-      if (!empty($block->getRegion())) {
-        $region = $block->getRegion();
+      $region_name_from_block = self::sanitize($block->getRegion());
+
+      /** @var array<string, mixed> $block_configuration */
+      $block_configuration = $elements['#configuration'];
+
+      if (!empty($region_name_from_block)) {
+        $region = $region_name_from_block;
       }
       else {
-        $region = (isset($variables['elements']['#configuration']['region'])) ? $variables['elements']['#configuration']['region'] : '';
+        $region = is_string($block_configuration['region'])
+          ? self::sanitize($block_configuration['region'])
+          : '';
       }
-      // Adds suggestion with region and block id.
-      $suggestions[] = 'block__' . $region . '__' . $variables['elements']['#id'];
-      // Adds suggestion with region id.
-      $suggestions[] = 'block__' . $region;
 
-      if (isset($variables['elements']['#base_plugin_id'])) {
-        // Adds suggestions with base and derivative plugin id.
-        $suggestions[] = 'block__' . $region . '__' . $variables['elements']['#base_plugin_id'];
+      $base_plugin_id = is_string($elements['#base_plugin_id'])
+        ? self::sanitize($elements['#base_plugin_id']) : '';
 
-        if ($variables['elements']['#derivative_plugin_id']) {
-          $suggestions[] = 'block__' . $region . '__' . $variables['elements']['#base_plugin_id'] . '__' . self::sanitize($variables['elements']['#derivative_plugin_id']);
+      $derivate_plugin_id = is_string($elements['#derivative_plugin_id'])
+        ? self::sanitize($elements['#derivative_plugin_id']) : '';
+
+      $route_name = is_string(\Drupal::routeMatch()->getRouteName())
+        ? self::sanitize(\Drupal::routeMatch()->getRouteName()) : '';
+
+      // Adds suggestions with region id.
+      if (!empty($region)) {
+        $suggestions[] = 'block__' . $region;
+
+        $suggestions[] = 'block__' . $region . '__' . $elements_id_original;
+
+        if (!empty($base_plugin_id)) {
+          // Adds suggestions with base and derivative plugin id.
+          $suggestions[] = 'block__' . $region . '__' . $base_plugin_id;
+
+          if (!empty($derivate_plugin_id)) {
+            $suggestions[] = 'block__' . $region . '__' . $base_plugin_id . '__' . $derivate_plugin_id;
+          }
         }
+      }
 
-        if (\Drupal::routeMatch()->getRouteName()) {
-          $route_name = str_replace('.', '_', \Drupal::routeMatch()->getRouteName());
-          $suggestions[] = $variables['theme_hook_original'] . '__' . $variables['elements']['#base_plugin_id'] . '__' . self::sanitize($route_name);
-        }
-
+      if (!empty($base_plugin_id) && !empty($route_name)) {
+        $suggestions[] = $hook_original . '__' . $base_plugin_id . '__' . $route_name;
       }
     }
   }
@@ -118,9 +179,19 @@ class Suggestions {
     $data_block = $attributes['data-block'] ?? [];
 
     if (isset($data_block['region'])) {
+      /** @var string $region */
       $region = $data_block['region'];
-      $suggestions[] = $variables['theme_hook_original'] . '__' . $region;
-      $suggestions[] = 'menu__' . $region;
+
+      /** @var string $hook_original */
+      $hook_original = $variables['theme_hook_original'];
+
+      if (!empty($region) && !empty($hook_original)) {
+        $suggestions[] = $hook_original . '__' . $region;
+      }
+
+      if (!empty($region)) {
+        $suggestions[] = 'menu__' . $region;
+      }
     }
   }
 
@@ -133,32 +204,31 @@ class Suggestions {
    *   Referenced $variables.
    */
   public static function page(array &$suggestions, array &$variables): void {
+    /** @var string $uri */
+    $uri = $_SERVER['REQUEST_URI'] ?? '';
+    $is_revision = str_contains($uri, 'revisions');
+
     // Add content type suggestions.
-    if (($node = \Drupal::request()->attributes->get('node')) &&
-      !strpos($_SERVER['REQUEST_URI'], "revisions")
-    ) {
-      if ($node instanceof NodeInterface) {
-        array_splice($suggestions, 1, 0, 'page__node__' . $node->getType());
-        $variables['content_type_name'] = $node->getType();
-      }
-    }
-    // Add taxonomy suggestions.
-    if (($term = \Drupal::request()->attributes->get('taxonomy_term')) &&
-      !strpos($_SERVER['REQUEST_URI'], "revisions")
-    ) {
-      if ($term instanceof Term) {
-        if ($term->get('parent')->target_id == 0) {
-          $suggestions[] = 'page__taxonomy__term__firstlevel';
-        }
-        else {
-          $suggestions[] = 'page__taxonomy__term__secondlevel';
-        }
-        $term_name = Html::getUniqueId(strtolower($term->getName()));
-        $suggestions[] = 'page__taxonomy__term__' . self::sanitize($term_name);
-        $suggestions[] = 'page__taxonomy__term__' . $term->bundle();
-      }
+    $node = \Drupal::request()->attributes->get('node');
+    if ($node instanceof NodeInterface && !$is_revision) {
+      array_splice($suggestions, 1, 0, 'page__node__' . $node->getType());
+      $variables['content_type_name'] = $node->getType();
     }
 
+    // Add taxonomy suggestions.
+    $term = \Drupal::request()->attributes->get('taxonomy_term');
+    if ($term instanceof Term && !$is_revision) {
+      if ((int) $term->get('parent')->target_id === 0) {
+        $suggestions[] = 'page__taxonomy__term__firstlevel';
+      }
+      else {
+        $suggestions[] = 'page__taxonomy__term__secondlevel';
+      }
+
+      $term_name = Html::getUniqueId(strtolower($term->getName()));
+      $suggestions[] = 'page__taxonomy__term__' . self::sanitize($term_name);
+      $suggestions[] = 'page__taxonomy__term__' . self::sanitize($term->bundle());
+    }
   }
 
   /**
@@ -174,31 +244,31 @@ class Suggestions {
     $item = $variables['item'];
 
     $entity = $item->getEntity();
+    $entity_type_id = $entity->getEntityTypeId();
+    $bundle = $entity->bundle();
 
-    $suggestions[] =
-      $variables['theme_hook_original'] . '__' . $entity->getEntityTypeId();
+    /** @var string $hook_original */
+    $hook_original = $variables['theme_hook_original'];
 
-    $suggestions[] =
-      $variables['theme_hook_original'] . '__' . $entity->bundle();
+    $suggestions[] = $hook_original . '__' . $entity_type_id;
 
-    $suggestions[] = $variables['theme_hook_original']
-      . '__' . $entity->getEntityTypeId() . '__' . $entity->bundle();
+    $suggestions[] = $hook_original . '__' . $bundle;
+
+    $suggestions[] = $hook_original . '__' . $entity_type_id . '__' . $bundle;
 
     if ($item->getParent()) {
       $field_name = $item->getParent()->getName();
-      $suggestions[] =
-        $variables['theme_hook_original'] . '__' . $field_name;
 
-      $suggestions[] = $variables['theme_hook_original']
-        . '__' . $entity->getEntityTypeId() . '__' . $field_name;
+      $suggestions[] = $hook_original . '__' . $field_name;
 
-      $suggestions[] = $variables['theme_hook_original']
-        . '__' . $entity->bundle() . '__' . $field_name;
+      $suggestions[] = $hook_original . '__' . $entity_type_id . '__' . $field_name;
+
+      $suggestions[] = $hook_original . '__' . $bundle . '__' . $field_name;
 
       $suggestions[] =
-        $variables['theme_hook_original']
-        . '__' . $entity->getEntityTypeId()
-        . '__' . $entity->bundle()
+        $hook_original
+        . '__' . $entity_type_id
+        . '__' . $bundle
         . '__' . $field_name;
     }
   }
@@ -212,7 +282,11 @@ class Suggestions {
    *   Referenced $variables.
    */
   public static function imageStyle(array &$suggestions, array $variables): void {
-    if (isset($variables['style_name'])) {
+    if (
+      isset($variables['style_name'], $variables['theme_hook_original'])
+      && is_string($variables['theme_hook_original'])
+      && is_string($variables['style_name'])
+    ) {
       $suggestions[] = $variables['theme_hook_original'] . '__' . $variables['style_name'];
     }
   }
@@ -227,6 +301,8 @@ class Suggestions {
    *   Clean string
    */
   public static function sanitize(string $s): string {
+    $s = strtolower($s);
+    $s = str_replace('.', '_', $s);
     return str_replace('-', '_', $s);
   }
 
