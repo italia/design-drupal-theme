4fb73c66028acfb731f725ad5c185a8efeac4999
arturu <arturu@arturu.it>
2026-04-10 06:00:27 +0200

    fix(storybook): upgrade

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -8,11 +8,11 @@ const serverUrl =
 const preview = {
   ...(serverUrl ? { server: { url: serverUrl } } : {}),
   parameters: {
-    // options: {
-    //   storySort: {
-    //     order: ['Bootstrap Italia', ['Content', 'Components', 'Utilities']],
-    //   },
-    // },
+    options: {
+      storySort: {
+        order: ['Bootstrap Italia', ['Content', 'Components', 'Utilities']],
+      },
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
